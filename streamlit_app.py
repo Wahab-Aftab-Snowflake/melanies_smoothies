e0b8bf39a82f04dc35aa1a29cbc218cf0a9a5ced
Wahab-Aftab-Snowflake <s.chishty@magnoos.com>
2025-08-19 10:32:39 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ if ingredients_list:
 
       
         st.subheader(fruit+" Nutrition Information")
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
-        sf_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
+        # sf_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredient_string + """','""" + name_on_order + """')"""
